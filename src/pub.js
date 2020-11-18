be9874592b0eaa88ad3fad4d592d3f64993467f0
unknown <iwbn@kaist.ac.kr>
2020-11-18 21:21:57 +0900

    add href to authors

--- src/pub.js
+++ src/pub.js
@@ -1,4 +1,11 @@
 function refine_pub(){
+	var author_info_list = document.getElementsByClassName("pub-author-info-list")[0].children;
+	var author_infos = [];
+	for (i = 0; i < author_info_list.length; i++) {
+		var item = author_info_list[i];
+		author_infos[item.innerHTML] = item.getAttribute("url");
+	}
+	
     var authors_arr = document.getElementsByClassName("pub-authors");
     for (i = 0; i < authors_arr.length; i++) {
         var authors = authors_arr[i].children;
@@ -6,6 +13,10 @@ function refine_pub(){
             if (authors[j].innerHTML == 'Woobin Im'){
                 authors[j].setAttribute('style', 'font-weight:bold')
             }
+			else if (typeof author_infos[authors[j].innerHTML] !== 'undefined'){
+				authors[j].outerHTML  = '<a href="' + author_infos[authors[j].innerHTML] +'">' + authors[j].outerHTML  + '</a>';
+				
+			}
             if (authors.length > 2 && j < authors.length - 1)
                 $(authors[j]).after(',');
             else if (authors.length != 1 && j == authors.length - 1)
