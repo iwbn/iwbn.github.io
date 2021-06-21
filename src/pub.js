7a244fde2b7e0f4250524fc04018fb7b1da7f3fb
unknown <iwbn@kaist.ac.kr>
2021-06-21 19:13:42 +0900

    fix namecard

--- src/pub.js
+++ src/pub.js
@@ -31,9 +31,22 @@ function refine_pub(){
     for (i = 0; i < pub_items.length; i++) {
         var pub_item = pub_items[i];
         var this_year = parseInt(pub_item.getAttribute("year"));
+		
+		var pub_journal = $(pub_item).children(".pub-journal")[0];
+		if (pub_journal){
+			var journal_name = $.trim(pub_journal.innerHTML);
+			pub_journal.innerHTML = journal_name;
+			var year_span = $("<span>");
+			year_span.addClass("only-on-print");
+			year_span.css("display", "inline");
+			year_span.html(", " + this_year.toString());
+			$(pub_journal).after(year_span);
+		}
+		
         if (this_year != prev_item_year){
 			$(pub_item).wrap('<div class="print-dont-break"></div>');
 			var year_item = $('<span class="pub-year">'+this_year.toString()+'</span>');
+			year_item.addClass("not-on-print");
             $(pub_item).before(year_item);
             var prev_item_year = parseInt(pub_item.getAttribute("year"));
         }
